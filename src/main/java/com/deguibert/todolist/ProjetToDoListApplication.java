0be180e1756c63149a2a7157fd92d612b0229d6c
2Guib <maxime.deguibert@orange.fr>
2022-05-06 20:41:46 +0200

    Modif de déploiement

--- src/main/java/com/deguibert/todolist/ProjetToDoListApplication.java
+++ src/main/java/com/deguibert/todolist/ProjetToDoListApplication.java
@@ -2,9 +2,10 @@ package com.deguibert.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjetToDoListApplication {
+public class ProjetToDoListApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetToDoListApplication.class, args);
